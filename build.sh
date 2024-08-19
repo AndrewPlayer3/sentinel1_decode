73a70bdba41d414531a419bd50cbf6d2011d2b4c
Andrew Player <andrewplayer3@gmail.com>
2024-08-19 10:44:29 -0500

    made include paths variables

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ mkdir bin 2>/dev/null
 echo "Compiling main to bin/main."
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	g++ -fopenmp -std=c++20 -O3 src/main.cpp src/packet.cpp src/packet_decoding.cpp src/decoding_utils.cpp -o bin/main
+	g++ -fopenmp -std=c++20 -O3 src/main.cpp src/packet.cpp src/perf.cpp src/decoding_utils.cpp -o bin/main
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	/opt/homebrew/opt/llvm/bin/clang++ -fopenmp -std=c++20 -O3 src/main.cpp src/packet.cpp src/perf.cpp src/decoding_utils.cpp -o bin/main
 else
@@ -18,10 +18,23 @@ fi
 
 echo "Compiling plotting to bin/plot"
 
+export PYTHON_INCLUDE="$HOME/miniforge3/envs/signal_processing/include/python3.12"
+export NUMPY_INCLUDE="$HOME/miniforge3/envs/signal_processing/lib/python3.12/site-packages/numpy/_core/include"
+
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	g++ -fopenmp -std=c++20 -O3 -w src/plotting.cpp src/packet.cpp src/decoding_utils.cpp -I/$HOME/miniforge3/envs/signal_processing/include/python3.12 -I$HOME/miniforge3/envs/signal_processing/lib/python3.12/site-packages/numpy/_core/include -lpython3.12 -o bin/plot
+
+	g++ -fopenmp -std=c++20 -O3 -w src/plotting.cpp src/packet.cpp src/decoding_utils.cpp -I$PYTHON_INCLUDE -I$NUMPY_INCLUDE -lfftw3 -lm -lpython3.12 -o bin/plot
+
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-	/opt/homebrew/opt/llvm/bin/clang++ -fopenmp -std=c++20 -O3 -w src/plotting.cpp src/packet.cpp src/decoding_utils.cpp -I/opt/homebrew/include -I/$HOME/miniforge3/envs/signal_processing/include/python3.12 -I$HOME/miniforge3/envs/signal_processing/lib/python3.12/site-packages/numpy/_core/include -L/opt/homebrew/lib -L/opt/homebrew/Frameworks/Python.framework/Versions/3.12/lib -lfftw3 -lm -lpython3.12 -o bin/plot
+
+	export PYTHON_INCLUDE="$HOME/miniforge3/envs/signal_processing/include/python3.12"
+	export NUMPY_INCLUDE="$HOME/miniforge3/envs/signal_processing/lib/python3.12/site-packages/numpy/_core/include"
+	export HOMEBREW_INCLUDE="/opt/homebrew/include"
+	export HOMEBREW_LIB="/opt/homebrew/lib"
+	export HOMEBREW_PYTHON_INCLUDE="/opt/homebrew/Frameworks/Python.framework/Versions/3.12/lib"
+
+	/opt/homebrew/opt/llvm/bin/clang++ -fopenmp -std=c++20 -O3 -w src/plotting.cpp src/packet.cpp src/decoding_utils.cpp -I$HOMEBREW_INCLUDE -I$PYTHON_INCLUDE -I$NUMPY_INCLUDE -L$HOMEBREW_LIB -L$HOMEBREW_PYTHON_INCLUDE -lfftw3 -lm -lpython3.12 -o bin/plot
+
 else
 	echo "$OSTYPE is not supported by this script, see the commands for what needs to be compiled."
 fi
