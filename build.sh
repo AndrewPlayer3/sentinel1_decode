70808571aba37070441c8cde11ace12c9f1135a4
Andrew Player <andrewplayer3@gmail.com>
2024-08-19 12:00:42 -0500

    fix

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ mkdir bin 2>/dev/null
 echo "Compiling main to bin/main."
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	g++ -fopenmp -std=c++20 -O3 src/main.cpp src/packet.cpp src/perf.cpp src/decoding_utils.cpp -o bin/main
+	g++ -fopenmp -std=c++20 -O3 src/main.cpp src/packet.cpp src/perf.cpp src/decoding_utils.cpp src/aux_decoding.cpp -o bin/main
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-	/opt/homebrew/opt/llvm/bin/clang++ -fopenmp -std=c++20 -O3 src/main.cpp src/packet.cpp src/perf.cpp src/decoding_utils.cpp -o bin/main
+	/opt/homebrew/opt/llvm/bin/clang++ -fopenmp -std=c++20 -O3 src/main.cpp src/packet.cpp src/perf.cpp src/decoding_utils.cpp src/aux_decoding.cpp -o bin/main
 else
 	echo "$OSTYPE is not supported by this script, see the commands for what needs to be compiled."
 fi
@@ -16,7 +16,7 @@ fi
 # See https://github.com/lava/matplotlib-cpp/blob/master/README.md for more information on matplotlib-cpp.
 # Needs to be linked to Python.h and <numpy>, which in my case are both in my conda environment.
 
-echo "Compiling plotting to bin/plot"
+echo "Compiling plotting to bin/plot."
 
 export PYTHON_INCLUDE="$HOME/miniforge3/envs/signal_processing/include/python3.12"
 export NUMPY_INCLUDE="$HOME/miniforge3/envs/signal_processing/lib/python3.12/site-packages/numpy/_core/include"
