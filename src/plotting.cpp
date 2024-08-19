c58e6fe0c0b846c80b6ae86bbcdb5b7366abcb4e
Andrew Player <andrewplayer3@gmail.com>
2024-08-19 02:55:34 -0500

    figuring out fftw3

--- src/plotting.cpp
+++ src/plotting.cpp
@@ -10,6 +10,8 @@ Description: Main function with random command-line arguments for testing purpos
 
 #include <thread>
 
+#include "fftw3.h"
+
 #include "../include/matplotlibcpp.h"
 
 #include "packet_decoding.hpp"
@@ -17,6 +19,24 @@ Description: Main function with random command-line arguments for testing purpos
 using namespace std;
 
 namespace plt = matplotlibcpp;
+
+
+void plot_complex_samples(const vector<complex<float>>& complex_samples, const bool& plot_real = true)
+{
+    vector<float> real_parts;
+    vector<float> imag_parts;
+
+    for (complex<float> sample : complex_samples)
+    {
+        real_parts.push_back(sample.real());
+        imag_parts.push_back(sample.imag());
+    }
+
+    plt::figure();
+    if (plot_real) plt::plot(real_parts);
+    else           plt::plot(imag_parts);
+    plt::show();
+}
 
 
 void plot_complex_samples(string filename, const int& packet_index, const bool& plot_real = true)
@@ -40,6 +60,56 @@ void plot_complex_samples(string filename, const int& packet_index, const bool& 
     if (plot_real) plt::plot(real_parts);
     else           plt::plot(imag_parts);
     plt::show();
+}
+
+
+
+vector<complex<float>> compute_1d_dft(const vector<complex<float>>& complex_signal)
+{
+    int dft_size = complex_signal.size();
+
+    fftw_complex *in, *out;
+    fftw_plan plan;
+
+    in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * dft_size);
+    out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * dft_size);
+
+    for (int i = 0; i < dft_size; i++)
+    {
+        in[i][0] = complex_signal[i].real();
+        in[i][1] = complex_signal[i].imag();
+    }
+
+    plan = fftw_plan_dft_1d(dft_size, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+
+    fftw_execute(plan);
+
+    vector<complex<float>> complex_signal_fft;
+    complex_signal_fft.reserve(dft_size);
+    for (int i = 0; i < dft_size; i++)
+    {
+        complex_signal_fft.push_back(complex<float>(out[i][0], out[i][1]));
+    }
+
+    fftw_destroy_plan(plan);
+
+    fftw_free(in); fftw_free(out);
+
+    return complex_signal_fft;
+}
+
+
+void plot_fft(string filename, const int& packet_index, const bool& plot_real = true)
+{
+    ifstream data = open_file(filename);
+
+    vector<L0Packet> packets = get_n_packets(data, packet_index + 1, false, 0);
+
+    vector<complex<float>> complex_samples = packets[packet_index].get_complex_samples();
+
+    vector<complex<float>> complex_samples_fft = compute_1d_dft(complex_samples);
+
+    plot_complex_samples(complex_samples_fft);
 }
 
 
@@ -114,6 +184,16 @@ int main(int argc, char* argv[])
         }
 
         plot_swath(string(argv[3]), stoi(argv[2]));
+    }
+    else if (command == "plot_fft")
+    {
+        if(argv[2] == __null || argv[3] == __null) 
+        {
+            cout << "Please enter the packet index and filename." << endl;
+            return 1;
+        }
+
+        plot_fft(string(argv[3]), stoi(argv[2]));
     }
     else
     {
