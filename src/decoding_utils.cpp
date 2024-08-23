9907293f297c985192c929865503c9bf6fc496e8
Andrew Player <andrewplayer3@gmail.com>
2024-08-23 13:52:04 -0500

    refactors

--- src/decoding_utils.cpp
+++ src/decoding_utils.cpp
@@ -14,15 +14,17 @@ Description: Functions to assist reading and decompressing binary data.
 
 /* Returns the next m_code using the relevant huffman tree for the given brc
    Checks length of bits in tree to handle edge cases but is slower than huffman_decode */
-u_int16_t huffman_decode_with_length(const vector<u_int8_t>& data, const int& brc, int& bit_index)
-{
+u_int16_t huffman_decode_with_length(
+    const vector<u_int8_t>& data,
+    const int& brc, 
+          int& bit_index
+) {
     vector<unordered_map<u_int16_t, u_int8_t>> huffman_coding = HUFFMAN_CODINGS_WITH_LENGTH[brc];
 
-    u_int16_t bits = -1;
+    u_int16_t bits     = -1;
+    u_int8_t  bit_len  = BRC_TO_HUFFMAN_START_BIT_LEN[brc];
+    u_int8_t  max_bits = 10;
 
-    int bit_len = BRC_TO_HUFFMAN_START_BIT_LEN[brc];
-
-    int max_bits = 10;
     for (int i = 0; i < max_bits; i++)
     {
         bits = read_n_bits(data, bit_index, bit_len);
@@ -30,10 +32,7 @@ u_int16_t huffman_decode_with_length(const vector<u_int8_t>& data, const int& br
         if (!huffman_coding[bit_len - 1].contains(bits)) bit_len += 1;
         else break;
 
-        if (bit_len > 10) 
-        {
-            throw out_of_range("Max bit length exceeded in Huffman decoding.");
-        }
+        if (bit_len > 10) throw out_of_range("Max bit length exceeded in Huffman decoding.");
     }
     bit_index += bit_len;
 
@@ -42,14 +41,16 @@ u_int16_t huffman_decode_with_length(const vector<u_int8_t>& data, const int& br
 
 
 /* Returns the next m_code using the relevant huffman tree for the given brc */
-u_int16_t huffman_decode(const vector<u_int8_t>& data, const int& brc, int& bit_index)
-{
-   unordered_map<u_int16_t, u_int8_t> huffman_coding = HUFFMAN_CODINGS[brc];
+u_int16_t huffman_decode(
+    const vector<u_int8_t>& data,
+    const int& brc, 
+          int& bit_index
+) {
+    unordered_map<u_int16_t, u_int8_t> huffman_coding = HUFFMAN_CODINGS[brc];
 
-    u_int16_t bits = -1;
-
-    int bit_len = BRC_TO_HUFFMAN_START_BIT_LEN[brc];
-
+    u_int16_t bits     = -1;
+    u_int8_t  bit_len  = BRC_TO_HUFFMAN_START_BIT_LEN[brc];
+    u_int8_t  max_bits = 10;
 
     if (brc == 4)
     {
@@ -60,8 +61,6 @@ u_int16_t huffman_decode(const vector<u_int8_t>& data, const int& brc, int& bit_
         }
         bit_len += 1;
     }
-
-    int max_bits = 10;
     for (int i = 0; i < max_bits; i++)
     {
         bits = read_n_bits(data, bit_index, bit_len);
@@ -81,24 +80,29 @@ u_int16_t huffman_decode(const vector<u_int8_t>& data, const int& brc, int& bit_
 
 
 /* Returns a vector of the next num_bytes bytes from a data stream */
-vector<u_int8_t> read_bytes(ifstream&  data, const int& num_bytes) 
-{
+vector<u_int8_t> read_bytes(
+    ifstream&  data,
+    const int& num_bytes
+) {
     vector<u_int8_t> buffer(num_bytes);
-
-    data.read(reinterpret_cast<char*>(buffer.data()), num_bytes);
-
+    data.read(
+        reinterpret_cast<char*>(buffer.data()),
+        num_bytes
+    );
     return buffer;
 }
 
 
 /* Returns the integer encoded by the n bits starting at start_bit in a vector of bytes */
-u_int64_t read_n_bits(const std::vector<u_int8_t>& data, const int& start_bit, const int& n)
-{
+u_int64_t read_n_bits(
+    const std::vector<u_int8_t>& data,
+    const int& start_bit,
+    const int& n
+) {
     if (n < 1 || n > 64) 
     {
         throw invalid_argument("Invalid number of bits to read. Must be between 1 and 64.");
     }
-
     int byte_index = start_bit / 8;
     int bit_offset = start_bit % 8;
 
@@ -106,7 +110,6 @@ u_int64_t read_n_bits(const std::vector<u_int8_t>& data, const int& start_bit, c
     {
         throw out_of_range("Start bit is outside of the data vector.");
     }
-
     u_int64_t result = 0;
     int bits_read = 0;
 
@@ -129,7 +132,6 @@ u_int64_t read_n_bits(const std::vector<u_int8_t>& data, const int& start_bit, c
         bit_offset = 0;
         byte_index++;
     }
-
     return result;
 }
 
--- src/decoding_utils.hpp
+++ src/decoding_utils.hpp
@@ -18,12 +18,27 @@ Description: Functions to assist reading and decompressing binary data.
 #include "structs.hpp"
 
 
-u_int16_t huffman_decode(const vector<u_int8_t>& data, const int& brc, int& bit_index);
+u_int16_t huffman_decode(
+    const vector<u_int8_t>& data,
+    const int& brc,
+          int& bit_index
+);
 
-u_int16_t huffman_decode_with_length(const vector<u_int8_t>& data, const int& brc, int& bit_index);
+u_int16_t huffman_decode_with_length(
+    const vector<u_int8_t>& data,
+    const int& brc,
+          int& bit_index
+);
 
-vector<u_int8_t> read_bytes(ifstream&  data, const int& num_bytes);
+vector<u_int8_t> read_bytes(
+    ifstream&  data,
+    const int& num_bytes
+);
 
-u_int64_t read_n_bits(const std::vector<u_int8_t>& data, const int& start_bit, const int& n);
+u_int64_t read_n_bits(
+    const std::vector<u_int8_t>& data,
+    const int& start_bit,
+    const int& n
+);
 
 ifstream open_file(const string& filename);
